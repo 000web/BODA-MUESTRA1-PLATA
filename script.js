7f21941b69e4203975eeb6c7ab06204e3c4d3f80
000web <101223443+000web@users.noreply.github.com>
2022-09-06 03:40:52 -0500

    Update+
    Update+

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-function calculateVh() {
-    var vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', vh + 'px');
-  }
-  
-  // Initial calculation
-  calculateVh();
-  
-  // Re-calculate on resize
-  window.addEventListener('resize', calculateVh);
-  
-  // Re-calculate on device orientation change
-  window.addEventListener('orientationchange', calculateVh);
+const documentHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+   }
+   window.addEventListener("resize", documentHeight)
+   documentHeight()
